1264ffe43725676163b8c74fbbf4bbd7bc7582c0
Taylor Dennis <taylrdnns@gmail.com>
2020-07-06 21:30:50 -0500

    [styles] make font bigger, change background color

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -8,7 +8,7 @@ export default ({ data }) => {
      <Layout>
    <div>
      <h1>{post.frontmatter.title}</h1>
-     <h4 style={{color: 'rgb(165, 164, 164)'}}><span style={{fontSize: '0.8em'}}> {post.frontmatter.date}</span></h4>
+     <h4><span> {post.frontmatter.date}</span></h4>
      <div dangerouslySetInnerHTML = {{ __html: post.html }}/>
    </div>
    </Layout>
